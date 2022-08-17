7c208545ff8c75c6bd6638b81f11ae2417f72376
Subudei <Subudei@gmail.com>
2022-08-17 20:32:05 +0600

    added txtfilecheckfunc

--- pkg/ascii/makemap.go
+++ pkg/ascii/makemap.go
@@ -2,6 +2,10 @@ package ascii
 
 import (
 	"bufio"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
@@ -54,4 +58,52 @@ func PrintAns(words []string, ascii map[rune][9]string) string {
 		}
 	}
 	return answer
+}
+
+func TxtFileCheck(fileName string) bool {
+	hashStandard := "e194f1033442617ab8a78e1ca63a2061f5cc07a3f05ac226ed32eb9dfd22a6bf"
+	hashShadow := "26b94d0b134b77e9fd23e0360bfd81740f80fb7f6541d1d8c5d85e73ee550f73"
+	hashThinkertoy := "a57beec43fde6751ba1d30495b092658a064452f321e221d08c3ac34a9dc1294"
+
+	file, err := os.Open("pkg/ascii/fonts/" + fileName)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer file.Close()
+	buf := make([]byte, 30*1024)
+	sha256 := sha256.New()
+	for {
+		n, err := file.Read(buf)
+		if n > 0 {
+			_, err := sha256.Write(buf[:n])
+			if err != nil {
+				fmt.Println(err)
+				return false
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("Read %d bytes: %v", n, err)
+			break
+		}
+	}
+	sum := fmt.Sprintf("%x", sha256.Sum(nil))
+	switch fileName {
+	case "shadow.txt":
+		if string(sum) == string(hashShadow) {
+			return true
+		}
+	case "standard.txt":
+		if string(sum) == string(hashStandard) {
+			return true
+		}
+	case "thinkertoy.txt":
+		if string(sum) == string(hashThinkertoy) {
+			return true
+		}
+	}
+	return false
 }
