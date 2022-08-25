b3cbb211db03fde271ce77cad454b71ab24c0da6
Subudei <Subudei@gmail.com>
2022-08-25 21:43:49 +0600

    added createhandler func

--- pkg/app/server.go
+++ pkg/app/server.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"ascii-art-web/pkg/ascii"
 	"net/http"
 	"text/template"
 )
@@ -33,6 +34,50 @@ func ViewHandler(writer http.ResponseWriter, request *http.Request) {
 	err = html.Execute(writer, nil)
 	if err != nil {
 		Errors(writer, "Page is Not Found", http.StatusNotFound)
+		return
+	}
+}
+
+func CreateHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("templates/answer.html")
+	if err != nil {
+		Errors(writer, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	if request.Method != http.MethodPost {
+		Errors(writer, "Method is not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	req := request.FormValue("req")
+	font := request.FormValue("font")
+
+	if req == "" {
+		Ascii.StatCode = 400
+		Errors(writer, "Bad Request. Write something", http.StatusBadRequest)
+		return
+	}
+
+	if font == "" {
+		Ascii.StatCode = 400
+		Errors(writer, "Bad Request. Choose any font", http.StatusBadRequest)
+		return
+	}
+
+	ans, statCode := ascii.ConvertAscii(req, font)
+	Ascii.StatCode = statCode
+	if Ascii.StatCode == 200 {
+		Ascii.Str = ans
+		Ascii.Orig = req
+	} else {
+		Errors(writer, ans, statCode)
+		return
+	}
+
+	err = html.Execute(writer, Ascii)
+	if err != nil {
+		http.Error(writer, "404: Not Found", 404)
 		return
 	}
 }
