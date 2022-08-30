188470d55285b24d8c0f170f78dedd0f76dbd9c5
Subudei <Subudei@gmail.com>
2022-08-30 11:15:30 +0600

    updated main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"ascii-art-web/pkg/app"
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.ViewHandler)
+	mux.HandleFunc("/ascii-art", app.CreateHandler)
+	mux.Handle("/templates/css/", http.StripPrefix("/templates/css/", http.FileServer(http.Dir("templates/css"))))
+	mux.Handle("/templates/img/", http.StripPrefix("/templates/img/", http.FileServer(http.Dir("templates/img"))))
+	fmt.Printf("Starting server at post: 8087\nhttp://localhost:8087/\n")
+	err := http.ListenAndServe("localhost:8087", mux)
+	log.Fatal(err)
 }
